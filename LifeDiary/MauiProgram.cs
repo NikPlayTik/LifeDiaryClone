561d97d36539994a3214577bcb000e9fdf38171b
Nikita Anishenco <tanishenco91@gmail.com>
2024-03-17 16:14:16 +0300

    Убрал цвет с панели состояния

--- LifeDiary/MauiProgram.cs
+++ LifeDiary/MauiProgram.cs
@@ -1,23 +1,38 @@
 ﻿using CommunityToolkit.Maui;
 using Microcharts.Maui;
+using Microsoft.Maui.LifecycleEvents;
 
-namespace LifeDiary;
-
-public static class MauiProgram
+namespace LifeDiary
 {
-	public static MauiApp CreateMauiApp()
-	{
-		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
-			.UseMicrocharts()
-			.UseMauiCommunityToolkit()
-			.ConfigureFonts(fonts =>
-			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-			});
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .UseMicrocharts()
+                .UseMauiCommunityToolkit()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                })
+                .ConfigureLifecycleEvents(events =>
+                {
+#if ANDROID
+                    events.AddAndroid(android => android.OnCreate((activity, bundle) => MakeStatusBarTranslucent(activity)));
 
-		return builder.Build();
-	}
+                    static void MakeStatusBarTranslucent(Android.App.Activity activity)
+                    {
+                        activity.Window.SetFlags(Android.Views.WindowManagerFlags.LayoutNoLimits, Android.Views.WindowManagerFlags.LayoutNoLimits);
+                        activity.Window.ClearFlags(Android.Views.WindowManagerFlags.TranslucentStatus);
+                        activity.Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+                    }
+#endif
+                });
+
+            return builder.Build();
+        }
+    }
 }
